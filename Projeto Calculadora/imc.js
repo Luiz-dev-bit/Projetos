42d73f5ffd0b151a8b0f614a89111a80a94b1803
Luiz-dev-bit <luizhenriquef46@gmail.com>
2024-08-03 23:34:42 -0300

    valor

--- Projeto Calculadora/imc.js
+++ Projeto Calculadora/imc.js
@@ -9,9 +9,29 @@ function imc () {
     const resultado = document.getElementById('resultado');
 
     if (nome !== '' && altura !== '' && peso !== '') {
-        alert ('valor preenchido!');
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso';
+        }else if (valorIMC < 25) {
+            classificacao = 'com peso ideal. Parabens!!!';
+        }else if(valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }else if(valorIMC < 35){
+            classificacao = 'com obesidade grau 1.';
+        }else if(valorIMC < 40){
+            classificacao = 'com obesidade grau 2.';
+        }else {
+            classificacao = 'com obesidade grau 3. Cuidado!!';
+        }
+
+         resultado.textContent = `${nome} seu IMC é ${valorIMC}, e você está ${classificacao}`;
+
     }else {
-        resultado.innerText = 'Preencha todos os campos!!!'
+        resultado.innerText = 'Preencha todos os campos!!!';
     }
 
 }
